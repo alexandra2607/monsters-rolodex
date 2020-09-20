f93471abc81b49c340c46580b1e1fcc303c4c662
Alexandra State <state.alexandra.gabriela@gmail.com>
2020-09-20 16:36:44 +0300

    Updates

---
+++ precache-manifest.ff87a2344d4b524e0fcc889549e1aaf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f196d275e0ac97e41d1c212cb5697c7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b8123ea49f6998414feb",
+    "url": "/monsters-rolodex/static/css/main.f6d8957c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8123ea49f6998414feb",
+    "url": "/monsters-rolodex/static/js/main.461bf68a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
